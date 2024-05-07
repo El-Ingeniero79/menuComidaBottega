be6600dcca43ddf15b9b27a8540b8a6da4cf82fb
El-Ingeniero79 <150141876+El-Ingeniero79@users.noreply.github.com>
2024-05-07 18:05:19 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-// mis variables
+// variables de mis menus
 let menus = {
     "a": {
         franjaHoraria: "Desayuno (7h - 12h)",
@@ -23,7 +23,7 @@ let menus = {
     }
 };
 
-// mi función para imprimir el menú de selección
+// mi Función para imprimir el menú de selección y que quede legible
 function imprimirMenuSeleccion(menu, tipoPlato) {
     let menuString = `Menú ${menu.franjaHoraria}\n\n${tipoPlato}:\n\n`;
     menu[tipoPlato].forEach((plato, index) => {
@@ -32,7 +32,7 @@ function imprimirMenuSeleccion(menu, tipoPlato) {
     return menuString;
 }
 
-// la función para seleccionar un plato
+// mi Función para seleccionar un plato despues de la interaccion del usuario
 function seleccionarPlato(menu, tipoPlato, index) {
     return {
         plato: menu[tipoPlato][index],
@@ -52,7 +52,7 @@ function comentarioIngenioso() {
     return comentarios[Math.floor(Math.random() * comentarios.length)];
 }
 
-// Función para calcular el precio total y la factura
+// Función para calcular el precio total, la factura y resumen de la comida
 function calcularPrecioTotal(primerPlatoIndex, segundoPlatoIndex, bebidaIndex, franjaHoraria) {
     let menu = menus[franjaHoraria];
     let primerPlato = seleccionarPlato(menu, "primerPlato", primerPlatoIndex - 1);
@@ -70,11 +70,14 @@ function calcularPrecioTotal(primerPlatoIndex, segundoPlatoIndex, bebidaIndex, f
     alert(mensaje);
 }
 
-// Función para pedir la franja horaria al usuario y saber el menu que corresponde
+// Función para pedir la franja horaria al usuario con gestion del error y explicacion de como seleccionar
 function pedirFranjaHoraria() {
     let franja;
     do {
-        franja = prompt("¿En qué franja horaria te encuentras?\n(a) 7 a 12h (Desayuno)\n(b) 13 a 17h (Comida)\n(c) 20 a 00h (Cena)").toLowerCase();
+        franja = prompt("¿En qué franja horaria te encuentras?\n (las opciones se eligen con a , b ó c sin parentesis)\n(a) 7 a 12h (Desayuno)\n(b) 13 a 17h (Comida)\n(c) 20 a 00h (Cena)").toLowerCase();
+        if (franja !== "a" && franja !== "b" && franja !== "c") {
+            alert("No te despistes has Seleccionado una opcion incorrecta. Por favor, elige un número válido.")
+        };
     } while (!menus[franja]);
     return franja;
 }
@@ -84,7 +87,7 @@ function validarSeleccionPlato(menu, tipoPlato, index) {
     return index >= 1 && index <= menu[tipoPlato].length;
 }
 
-// Orden de ejecución del código
+// Orden de ejecución del código para visionado correcto del ususario
 alert("¡Bienvenido a nuestro fantástico restaurante!");
 alert("Queremos darle un buen servicio, por lo tanto le solicitaremos en que horario vas a comer, para que no haya confusiones con los menus. Tambien podra consultar nuestros platos y precios para no llevarse sorpresas negativas. Le recordamos que todos nuestros precios llevan el IVA incluido. En caso de tener alguna duda esctribanos a: \n elMejor_RestauranteMundial@comebien.com ");
 let franjaHoraria = pedirFranjaHoraria();
